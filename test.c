e0e739b4a2f8d7a13a68ce41a5b23f4da7a26bd1
DisappearDay <117563652+DisappearDay@users.noreply.github.com>
2023-04-03 18:48:15 +0800

    Update test.c

--- test.c
+++ test.c
@@ -114,3 +114,73 @@ int main() {
 	}
 	return 0;
 }
+
+//int main() {
+//
+//	printf("请输入金额->:");
+//	int count = 0;
+//	scanf("%d", &count);
+//	int empty = 0;
+//	int total = 0;
+//
+//	if (count == 0) {
+//		total = 0;
+//	}
+//	else {
+//		total = 2 * count - 1;
+//	}
+//
+//
+//	//total = count;
+//	//empty = count;
+//	//while(empty >= 2) {
+//	//	total += empty / 2;
+//	//	empty = empty / 2 + empty % 2;
+//	//}
+//
+//
+//
+//	printf("能喝%d瓶饮料", total);
+//	return 0;
+//
+//}
+
+void print(int arr[],int sz) {
+	int i = 0;
+	for (i = 0; i < sz;i++) {
+		printf("%d ", arr[i]);
+	}
+}
+
+void move(int arr[], int sz) {
+	int left = 0;
+	int right = sz - 1;
+	while (left<right) {
+		while ((left < right)&&(arr[left] % 2 == 1)) {
+			left++;
+		}
+		while ((left < right) && (arr[right] % 2 == 0)) {
+			right--;
+		}
+		if (left < right) {
+			int temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
+		}
+	}
+
+}
+
+
+//输入一个数组，把数组的元素奇数位放前面，偶数位放后面
+int main() {
+	//int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int arr[] = { 1,3,5,7,9 };
+
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	move(arr,sz);
+	print(arr,sz);
+
+	return 0;
+}
+
